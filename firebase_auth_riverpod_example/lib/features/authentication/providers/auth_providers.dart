7ab35f50321a48d414bda04534cfb200c2f972a3
VincentFreeman <43032514+ShinHyunHo8149@users.noreply.github.com>
2023-04-03 14:52:01 +0900

    Create auth_providers.dart

---
+++ firebase_auth_riverpod_example/lib/features/authentication/providers/auth_providers.dart
@@ -0,0 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../models/firebase_auth_methods.dart';
+
+final authenticationProvider = Provider<FirebaseAuthMethods>((ref) {
+  return FirebaseAuthMethods(FirebaseAuth.instance);
+});
