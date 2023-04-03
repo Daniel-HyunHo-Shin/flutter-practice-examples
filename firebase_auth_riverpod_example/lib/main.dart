2f0633be29c6403ace4c23f5bd4bff116860827a
VincentFreeman <43032514+ShinHyunHo8149@users.noreply.github.com>
2023-04-03 12:45:15 +0900

    Creating Screen

--- firebase_auth_riverpod_example/lib/main.dart
+++ firebase_auth_riverpod_example/lib/main.dart
@@ -1,22 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
+  // Ensure flutter framework to be initialized
   WidgetsFlutterBinding.ensureInitialized();
-
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
-class MainApp extends StatelessWidget {
+//  Firebase 실행확인을 Riverpod를 통해 확인한다.
+// 이 방법으로 Loading / error state를 구현할 수 있다.
+final firebaseinitializerProvider = FutureProvider<FirebaseApp>((ref) async {
+  return await Firebase.initializeApp();
+});
+
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final initialize = ref.watch(firebaseinitializerProvider);
+
+    // Go router package를 이용할 것이므로 MaterialApp.router
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
     );
   }
 }
