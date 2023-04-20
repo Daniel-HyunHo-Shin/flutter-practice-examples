1f3ec7e21c9e36a78d0b0530ee17c43eca5dc916
VincentFreeman <43032514+ShinHyunHo8149@users.noreply.github.com>
2023-04-20 11:57:26 +0900

    commenting

--- flashcard/lib/features/flashcard/views/flash_card_feed_Item.dart
+++ flashcard/lib/features/flashcard/views/flash_card_feed_Item.dart
@@ -3,20 +3,27 @@ import '../../../common_widgets/flashcard_audio_player.dart';
 import '../models/flashcard.dart';
 
 class FlashcardFeedItem extends StatefulWidget {
-  final Flashcard flashcard;
-  final PageController pageController;
+  final Flashcard
+      flashcard; // The flashcard object containing the question, answer, and image/audio URLs
+  final PageController
+      pageController; // The page controller used for navigating between flashcards
 
-  const FlashcardFeedItem(
-      {super.key, required this.flashcard, required this.pageController});
+  const FlashcardFeedItem({
+    Key? key,
+    required this.flashcard,
+    required this.pageController,
+  }) : super(key: key);
 
   @override
   _FlashcardFeedItemState createState() => _FlashcardFeedItemState();
 }
 
 class _FlashcardFeedItemState extends State<FlashcardFeedItem> {
+  // Boolean to keep track of whether the card is flipped
   bool _isFlipped = false;
 
   void _goToNextPage() {
+    // Function to navigate to the next flashcard using the page controller
     widget.pageController.nextPage(
       duration: const Duration(milliseconds: 300),
       curve: Curves.easeInOut,
@@ -24,6 +31,7 @@ class _FlashcardFeedItemState extends State<FlashcardFeedItem> {
   }
 
   void _onAudioToggle() {
+    // Function to handle the audio player toggle
     setState(() {});
   }
 
@@ -31,31 +39,37 @@ class _FlashcardFeedItemState extends State<FlashcardFeedItem> {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
+        // Toggle the flip state of the card when tapped
         setState(() {
           _isFlipped = !_isFlipped;
         });
       },
       child: Stack(
         children: [
+          // The flashcard image as a background for the text
           SizedBox.expand(
             child: Image.network(
               widget.flashcard.imageUrl,
               fit: BoxFit.cover,
             ),
           ),
+          // A semi-transparent overlay to darken the image
           Container(color: Colors.black.withOpacity(0.3)),
+          // The text of the question or answer in the center of the card
           Center(
             child: Padding(
               padding: const EdgeInsets.all(16.0),
               child: Text(
                 _isFlipped
-                    ? widget.flashcard.answer
-                    : widget.flashcard.question,
+                    ? widget.flashcard
+                        .answer // If the card is flipped, show the answer
+                    : widget.flashcard.question, // If not, show the question
                 style: const TextStyle(fontSize: 32, color: Colors.white),
                 textAlign: TextAlign.center,
               ),
             ),
           ),
+          // An audio player for the flashcard audio, with a toggle button
           Positioned(
             top: 16,
             right: 16,
@@ -65,6 +79,7 @@ class _FlashcardFeedItemState extends State<FlashcardFeedItem> {
             ),
           ),
           if (_isFlipped)
+            // If the card is flipped, show pass/fail buttons
             Positioned(
               bottom: 16,
               left: 0,
