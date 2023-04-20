23b1395d1843f96dd788a0e0950b6b05ede1fd18
VincentFreeman <43032514+ShinHyunHo8149@users.noreply.github.com>
2023-04-20 13:31:41 +0900

    Update main.dart

--- flashcard/lib/main.dart
+++ flashcard/lib/main.dart
@@ -1,28 +1,39 @@
+// Import necessary packages and libraries
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// Import app-specific code
 import 'firebase_options.dart';
 import 'router/router.dart';
 
+// The main function that starts the app
 void main(List<String> args) async {
+  // Make sure Flutter is initialized before using any plugins or packages
   WidgetsFlutterBinding.ensureInitialized();
+  // Initialize Firebase with the default options for the current platform
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  // Use Riverpod to create the app's main widget tree and provide state
   runApp(const ProviderScope(child: MainApp()));
 }
 
+// The main app widget
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // Create a MaterialApp that uses Riverpod's router for navigation
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
+      // Customize the app's theme
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.grey),
       ),
+      // Use the app's GoRouter instance to manage navigation
       routerConfig: goRouter,
     );
   }
