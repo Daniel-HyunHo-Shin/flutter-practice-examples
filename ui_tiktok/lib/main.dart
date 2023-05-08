54bbffc9eeb2201658014377f1b601d741429572
VincentFreeman <43032514+ShinHyunHo8149@users.noreply.github.com>
2023-05-09 03:36:10 +0900

    init

--- ui_tiktok/lib/main.dart
+++ ui_tiktok/lib/main.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
 
+import 'homepage.dart';
+
+void main(List<String> args) {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData().copyWith(brightness: Brightness.dark),
+      home: const HomePage(),
+    );
+  }
+}
