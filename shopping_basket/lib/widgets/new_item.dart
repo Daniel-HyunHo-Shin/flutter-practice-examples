47f60e9a8e2bc9f068039c6f6f5db9ab18e741fb
VincentFreeman <43032514+ShinHyunHo8149@users.noreply.github.com>
2023-05-13 20:07:25 +0900

    Update new_item.dart

--- shopping_basket/lib/widgets/new_item.dart
+++ shopping_basket/lib/widgets/new_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_basket/data/categories.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -14,8 +15,76 @@ class _NewItemState extends State<NewItem> {
       appBar: AppBar(
         title: const Text('Add a new item'),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(12),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: const InputDecoration(
+                  label: Text('Name'),
+                ),
+                validator: (value) {
+                  return '';
+                },
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('Quantity'),
+                      ),
+                      initialValue: '1',
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      items: [
+                        for (final cateogry in categories.entries)
+                          DropdownMenuItem(
+                            value: cateogry.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: cateogry.value.typeColor,
+                                ),
+                                const SizedBox(
+                                  width: 6,
+                                ),
+                                Text(cateogry.value.type)
+                              ],
+                            ),
+                          )
+                      ],
+                      onChanged: (value) {},
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('Reset'),
+                  ),
+                  const SizedBox(width: 4),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('Reset'),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
