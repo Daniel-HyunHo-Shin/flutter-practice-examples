403d8fca422d76367eb46e32bf743706273ca308
VincentFreeman <43032514+ShinHyunHo8149@users.noreply.github.com>
2023-07-03 22:44:01 +0900

    create a new amplify trips

---
+++ amplify_trips/README.md
@@ -0,0 +1,3 @@
+# amplify_trips
+
+A new Flutter project.
---
+++ amplify_trips/android/app/src/main/kotlin/com/example/amplify_trips/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.amplify_trips
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
