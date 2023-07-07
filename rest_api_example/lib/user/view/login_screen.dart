7a21e2bbbbbfccce9d0302bfefa76f0a600497d2
VincentFreeman <43032514+ShinHyunHo8149@users.noreply.github.com>
2023-07-07 19:58:24 +0900

    Dio로 Auth API 요청해보기

--- rest_api_example/lib/user/view/login_screen.dart
+++ rest_api_example/lib/user/view/login_screen.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:rest_api_example/common/component/custom_text_form_field.dart';
 import 'package:rest_api_example/common/const/colors.dart';
@@ -8,6 +12,13 @@ class LoginSceen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dio = Dio();
+    // localHost
+    const emulatorIp = '10.0.2.2:3000';
+    const simulatorIp = '127.0.0.1:3000';
+
+    final ip = Platform.isIOS ? simulatorIp : emulatorIp;
+
     return DefaultLayout(
       child: SingleChildScrollView(
         keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
@@ -37,14 +48,35 @@ class LoginSceen extends StatelessWidget {
                 ),
                 const SizedBox(height: 16.0),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    // id: password
+                    const rawString = 'test@codefactory.ai:testtest';
+                    Codec<String, String> stringToBase64 = utf8.fuse(base64);
+                    String token = stringToBase64.encode(rawString);
+
+                    final resp = await dio.post('http://$ip/auth/login',
+                        options: Options(
+                            headers: {'authorization': 'Basic $token'}));
+
+                    print(resp.data);
+                  },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: primaryColor,
                   ),
                   child: const Text('로그인'),
                 ),
                 TextButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    const refreshToken =
+                        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RAY29kZWZhY3RvcnkuYWkiLCJzdWIiOiJmNTViMzJkMi00ZDY4LTRjMWUtYTNjYS1kYTlkN2QwZDkyZTUiLCJ0eXBlIjoicmVmcmVzaCIsImlhdCI6MTY4ODcyNzQ2NCwiZXhwIjoxNjg4ODEzODY0fQ.55Z5pV7gpeFo-J17r5wl88Q9oxK4AKAH8g3m8YYMJHI';
+                    final resp = await dio.post(
+                      'http://$ip/auth/token',
+                      options: Options(
+                        headers: {'authorization': 'Bearer $refreshToken'},
+                      ),
+                    );
+                    print(resp.data);
+                  },
                   style: TextButton.styleFrom(
                     textStyle: const TextStyle(color: primaryColor),
                   ),
